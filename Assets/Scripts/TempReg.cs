477bff6d446cbc7a9e3c1751db8ffdc5dfcc3f00
Aodh <Francie.Roessler@web.de>
2023-01-18 16:53:08 +0100

    Indicator, TempRange Asset added without logic

--- Assets/Scripts/TempReg.cs
+++ Assets/Scripts/TempReg.cs
@@ -1,96 +1,108 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
 using Cinemachine;
+using TMPro;
+using UnityEngine;
 
 // Von InteractableObject erben lassen
 public class TempReg : CameraTriggerZone
 {
- GameManager gameManager;
+    GameManager gameManager;
 
-[SerializeField] bool adjustable;
-[SerializeField] TextMeshPro text;
-[SerializeField] GameObject Obj;
- private int increasing = 0;
- private string stringy; 
- [SerializeField] CinemachineVirtualCamera temperCam;
- [SerializeField] GameObject tongsContainer;
+    [SerializeField]
+    bool adjustable;
 
+    [SerializeField]
+    TextMeshPro text;
 
+    [SerializeField]
+    GameObject Obj;
 
+    private int increasing = 0;
 
-      void Awake(){
+    private string stringy;
+
+    [SerializeField]
+    CinemachineVirtualCamera temperCam;
+
+    [SerializeField]
+    GameObject tongsContainer;
+
+    void Awake()
+    {
         gameManager = FindObjectOfType<GameManager>();
-      
     }
-    public override void OnClick()
 
-    {   
-        
+    public override void OnClick()
+    {
         base.OnClick();
         adjustable = true;
         tongsContainer.SetActive(false);
-    
-       
-             if (gameManager != null)
-         {
-             if (!isInteractable){
-                 gameManager.IncreaseErrorCounter();
-             }
-         
-         } else {
-             Debug.Log("gameManager = null");
-         }
-     }
 
-     public void Update(){
-        if (adjustable){
-            changeTemperature();
-          
-            //triggers stopIncrease when rechtsklick, vll was anderes mit UI?
-                 if (Input.GetMouseButtonDown(0) && increasing ==220){
-                    StopInc();
-          }
-          if(Input.GetMouseButtonDown(0) && increasing !=220){
-            gameManager.IncreaseErrorCounter();
-          }
+        if (gameManager != null)
+        {
+            if (!isInteractable)
+            {
+                gameManager.IncreaseErrorCounter();
+            }
         }
-   
-     }
+        else
+        {
+            Debug.Log("gameManager = null");
+        }
+    }
 
+    public void Update()
+    {
+        if (adjustable)
+        {
+            changeTemperature();
 
-public void changeTemperature(){
-    
-    
-   // Debug.Log("DAKJSLÖDJSALKJDALKSJDLKAJDLKWAJDWJLKWAJDÖLWAJöd");
-      
-      if (Input.GetAxis("Mouse ScrollWheel") >0 && increasing >=0 && increasing <750) {
-         Obj.transform.Rotate(Vector3.back * 10f, Space.Self);
-  
-        increasing +=10;
-        text.SetText(increasing + "°");
+            //triggers stopIncrease when rechtsklick, vll was anderes mit UI?
+            if (Input.GetMouseButtonDown(0) && increasing == 220)
+            {
+                StopInc();
+            }
+            if (Input.GetMouseButtonDown(0) && increasing != 220)
+            {
+                gameManager.IncreaseErrorCounter();
+            }
+        }
+    }
 
-        
+    public void changeTemperature()
+    {
+        // Debug.Log("DAKJSLÖDJSALKJDALKSJDLKAJDLKWAJDWJLKWAJDÖLWAJöd");
+        if (
+            Input.GetAxis("Mouse ScrollWheel") > 0 &&
+            increasing >= 0 &&
+            increasing < 750
+        )
+        {
+            Obj.transform.Rotate(Vector3.back * 10f, Space.Self);
 
-  
-}
-if (Input.GetAxis("Mouse ScrollWheel") < 0 && increasing >0 && increasing<=750) {
-      Obj.transform.Rotate(Vector3.forward * 10f, Space.Self);
-    
-        increasing -=10;
-        text.SetText(increasing + "°");
-       
-}}
+            increasing += 10;
+            text.SetText(increasing + "°");
+        }
+        if (
+            Input.GetAxis("Mouse ScrollWheel") < 0 &&
+            increasing > 0 &&
+            increasing <= 750
+        )
+        {
+            Obj.transform.Rotate(Vector3.forward * 10f, Space.Self);
 
+            increasing -= 10;
+            text.SetText(increasing + "°");
+        }
+    }
 
-
-public void StopInc(){
-
-    adjustable= false;
-    CameraSwitch.Instance.SwitchCamera(temperCam);
-    gameManager.SetNextSection();
-    Debug.Log("nopedidy");
-    tongsContainer.SetActive(true);
-}
+    public void StopInc()
+    {
+        adjustable = false;
+        gameManager.SetNextSection();
+        Debug.Log("nopedidy");
+        CameraSwitch.Instance.SwitchCamera (temperCam);
+        tongsContainer.SetActive(true);
+    }
 }
