659d27e570675f6032e375b092fcdbb741988b46
Zockbock <leiv.wolter@gmx.de>
2023-02-08 12:17:07 +0100

    added container for anlassofen

--- Assets/Scripts/KnifeContainer1.cs
+++ Assets/Scripts/KnifeContainer1.cs
@@ -127,7 +127,7 @@ public class KnifeContainer1 : InteractableObject
 
                     knife.transform.parent = transform;
                     Vector3 rotation = knife.transform.localEulerAngles;
-                    knife.GetComponent<Knife>().MoveKnife(new Vector3(0, -0.48f, 0), new Vector3(90, rotation.y, rotation.z));
+                    knife.GetComponent<Knife>().MoveKnife(new Vector3(0, -0.75f, 0.5f), new Vector3(90, rotation.y, -246));
                    // empties.transform.position = new Vector3(1.6f,3.16f,7.45f);
                    // transform.position = Vector3.down*100;
                     //knife.transform.localPosition = new Vector3(0, -0.48f, 0);
